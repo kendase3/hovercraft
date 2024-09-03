746bdd5dd480d87d92c4014e85c5972ec2e339c2
Steven 'Steve' Kendall <skend@google.com>
2024-09-03 23:18:27 +0000

    following tutorial mentioned in readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill whole browser window
+                fit_canvas_to_parent: true,
+                // don't listen to keyboard shortcuts like F keys, ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
