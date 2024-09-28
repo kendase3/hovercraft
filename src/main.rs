a84a980a03621844d6c8829ec3596e46963d8494
Steven 'Steve' Kendall <skend@google.com>
2024-09-28 19:03:22 -0400

    now shows a box!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
     App::new()
@@ -12,5 +12,24 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
+        .add_systems(Startup, (startup, spawn_player))
         .run();
+}
+
+fn startup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1.,1.)),
+            ..default()
+        },
+        ..default()
+    });
 }
