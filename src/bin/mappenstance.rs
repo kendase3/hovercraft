d7defe3972633b3f7a2bbdedc4242f1f09f09258
Steven 'Steve' Kendall <skend@google.com>
2024-11-07 13:17:18 -0500

    still very broken but closer

--- src/bin/mappenstance.rs
+++ src/bin/mappenstance.rs
@@ -108,16 +108,21 @@ fn main() {
     // then create a 2d array of cells with some filled, some empty
     // then render it from above
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                // fill whole browser window
-                fit_canvas_to_parent: true,
-                // don't listen to keyboard shortcuts like F keys, ctrl+R
-                prevent_default_event_handling: false,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins.set(
+                WindowPlugin {
+                    primary_window: Some(Window {
+                        // fill whole browser window
+                        fit_canvas_to_parent: true,
+                        // don't listen to keyboard shortcuts like F keys, ctrl+R
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }),
+                    ..default()
+                }
+            )
+                //ImagePlugin::default_nearest()))
+        )
         .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
         .add_systems(Startup, startup)
         //.add_systems(Update, move_player)
