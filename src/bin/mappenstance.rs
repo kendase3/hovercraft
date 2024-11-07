763b96b7b8cce7c90065b0e634d3a21cbd4ebedf
Steven 'Steve' Kendall <skend@google.com>
2024-11-07 13:59:01 -0500

    made mappenstance the orthographic demo for this version of bevy for now; had some early issues trying to use bevy main examples

--- src/bin/mappenstance.rs
+++ src/bin/mappenstance.rs
@@ -1,125 +1,78 @@
-use bevy::{
-    color::palettes::basic::SILVER,
-    prelude::*,
-    render::{
-        render_asset::RenderAssetUsages,
-        render_resource::{Extent3d, TextureDimension, TextureFormat},
-    },
-};
-use std::f32::consts::PI;
+//! Shows how to create a 3D orthographic view (for isometric-look games or CAD applications).
 
-fn startup(
-   mut commands: Commands,
-   mut meshes: ResMut<Assets<Mesh>>,
-   mut images: ResMut<Assets<Image>>,
-   mut materials: ResMut<Assets<StandardMaterial>>,
-   ) {
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
-        ..default()
-    });
-    let shapes = [
-        meshes.add(Cuboid::default()),
-    ];
-
-    let extrusions = [
-        meshes.add(Extrusion::new(Rectangle::default(), 1.)),
-    ];
-
-    let num_shapes = shapes.len();
-
-    /*
-    for (i, shape) in shapes.into_iter().enumerate() {
-        commands.spawn((
-            Mesh(shape),
-            MeshMaterial(debug_material.clone()),
-            // then the tutorial had some kind of transform i'm skipping
-            Shape,
-        ));
-    }
-    let num_extrusions = extrusions.len();
-    for (i, shape) in extrusions.into_iter().enumerate() {
-        commands.spawn((
-            Mesh(shape),
-            MeshMaterial(debug_material.clone()),
-            Shape,
-        ));
-    }
-    */
-
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            intensity: 10_000_000.,
-            range: 100.0,
-            shadow_depth_bias: 0.2,
-            ..default()
-        },
-        Transform::from_xyz(8.0, 16.0, 8.0),
-    ));
-
-    // ground plane
-    /*
-    commands.spawn((
-        Mesh(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10))),
-        MeshMaterial(materials.add(Color::from(SILVER))),
-        ));
-    */
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 7., 14.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
-    ));
-}
-
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::RENDER_WORLD,
-        )
-}
-
-// let's do a top-down view of a 8x8 grid, the height of the viewing window
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
-    // first find height of viewing window
-    // then divide it into 8x8 cells
-    // then create a 2d array of cells with some filled, some empty
-    // then render it from above
     App::new()
-        .add_plugins(
-            DefaultPlugins.set(
-                WindowPlugin {
-                    primary_window: Some(Window {
-                        // fill whole browser window
-                        fit_canvas_to_parent: true,
-                        // don't listen to keyboard shortcuts like F keys, ctrl+R
-                        prevent_default_event_handling: false,
-                        ..default()
-                    }),
-                    ..default()
-                },
-        ))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill whole browser window
+                fit_canvas_to_parent: true,
+                // don't listen to keyboard shortcuts like F keys, ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
-        .add_systems(Startup, startup)
+        .add_systems(Startup, setup)
         //.add_systems(Update, move_player)
         .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // camera
+    commands.spawn(Camera3dBundle {
+        projection: OrthographicProjection {
+            // 6 world units per window height.
+            scaling_mode: ScalingMode::FixedVertical(6.0),
+            ..default()
+        }
+        .into(),
+        transform: Transform::from_xyz(5.0, 5.0, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(5.0, 5.0)),
+        material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+        ..default()
+    });
+    // cubes
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::default()),
+        material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+        transform: Transform::from_xyz(1.5, 0.5, 1.5),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::default()),
+        material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+        transform: Transform::from_xyz(1.5, 0.5, -1.5),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::default()),
+        material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+        transform: Transform::from_xyz(-1.5, 0.5, 1.5),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::default()),
+        material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
+        transform: Transform::from_xyz(-1.5, 0.5, -1.5),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(3.0, 8.0, 5.0),
+        ..default()
+    });
 }
