17f921ef37e8b126ecf97c1db34dbaca00c9b74e
Steven 'Steve' Kendall <skend@google.com>
2024-11-13 02:07:32 +0000

    work in progress

--- src/bin/astar.rs
+++ src/bin/astar.rs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// a star implementation 
+/// a star implementation
+
+struct Mapp {}
 
 fn main() {
     println!("Hello, world!");
