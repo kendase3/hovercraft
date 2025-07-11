fb6d71879b420653e63861081ba3f28b84dd0259
Steven 'Steve' Kendall <skend@google.com>
2025-07-11 19:45:29 -0400

    general contract for the function has been established

--- src/laser.rs
+++ src/laser.rs
@@ -1,6 +1,8 @@
+use bevy::prelude::*;
 
-pub fn get_laser_vertices() 
-
--> Option<(Vec<[f32; 3]>, Vec<[f32; 3]>, Vec<[f32; 3]>)> {
+pub fn get_laser_vertices(
+    origin: Vec2,
+    dest: Vec2,
+) -> Option<(Vec<[f32; 3]>, Vec<[f32; 3]>, Vec<[f32; 3]>)> {
     None
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod physics;
 pub mod laser;
+pub mod physics;
