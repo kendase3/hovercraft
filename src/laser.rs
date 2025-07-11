0ed5963de3ee5667ef41c85dfbc58697463b5eba
Steven 'Steve' Kendall <skend@google.com>
2025-07-11 19:49:20 -0400

    so far so good

--- src/laser.rs
+++ src/laser.rs
@@ -1,8 +1,14 @@
+use crate::physics;
+
 use bevy::prelude::*;
 
 pub fn get_laser_vertices(
-    origin: Vec2,
-    dest: Vec2,
+    laser_origin: Vec2,
+    laser_dest: Vec2,
 ) -> Option<(Vec<[f32; 3]>, Vec<[f32; 3]>, Vec<[f32; 3]>)> {
+    let coordpair = physics::CoordPair {
+        center: laser_origin,
+        exterior: laser_dest,
+    };
     None
 }
